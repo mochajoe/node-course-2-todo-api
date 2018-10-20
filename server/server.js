7eaa24f4151d73d62fa8845a8c55160d25f675c1
Lala <jieyingpeng010@gmail.com>
2018-10-19 23:04:45 -0700

    Update server.js

--- server/server.js
+++ server/server.js
@@ -72,7 +72,7 @@ app.delete('/todos/:id', (req, res) => {
 
 app.patch('/todos/:id', (req, res) => {
   var id = req.params.id;
-  var body = _.pick(req.body, ['text', 'completed']);
+  var body = _.pick(req.body, ['text', 'completed', 'date']);
 
   if (!ObjectID.isValid(id)) {
     return res.status(404).send();
